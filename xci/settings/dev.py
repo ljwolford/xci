0894346b5c7c619d2aa95421ceae92e1bc19559c
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-05-20 10:51:33 -0400

    set quiz results to display results from default profile

--- xci/settings/dev.py
+++ xci/settings/dev.py
@@ -5,12 +5,12 @@ SECRET_KEY = 'd\xfd\x82\xc6\x88\x8e\x95k>\xa2\xe0GKN\xe3\xe5^\x99\xa9\n\xf4\x88o
 DOMAIN_NAME = 'http://localhost:5000'
 
 DEFAULT_PROFILE = {
-    "name" : "Default LRS Profile",
-    "endpoint" : "http://localhost:8000/xapi/",
-    # "endpoint" : "https://lrs.adlnet.gov/xapi/",
-    "auth" : "Basic %s" % base64.b64encode("%s:%s" % ("tom", "1234")),
-    "username" : "tom",
-    "password" : "1234"
+    "name" : "LRS",
+    # "endpoint" : "http://localhost:8000/xapi/",
+    "endpoint" : "https://lrs.adlnet.gov/xapi/",
+    "auth" : "Basic %s" % base64.b64encode("%s:%s" % ("lou", "password")),
+    "username" : "lou",
+    "password" : "password"
 }
 
 ALLOWED_BADGE_EXTENSIONS = set(['png'])
